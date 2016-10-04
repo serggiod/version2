1dbab8d466da0660cf9b7aff261cb913bd8dc89b
Orlando Sergio Dominguez <serggio@msn.com>
2016-10-04 11:56:41 -0300

    Versión 0.0.20: Cambios en el home.

--- public/controllers/homeController.js
+++ public/controllers/homeController.js
@@ -1,4 +1,15 @@
 angular
 		.module('legislaturaweb')
 		.controller('homeController',function($scope,$http){
+
+			$('#scrollBoletin').marquee({
+				enable : true,  // activa plug-in
+				direction: 'vertical', //direccion de scroll
+				itemSelecter : 'li',  //selecciona hijos
+				delay: 3000, //tiempo de espera
+				speed: 1, //velocidad
+				timing: 1,
+				mouse: true //si se detiene al pasar el mouse
+			});
+
 		});

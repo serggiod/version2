12bf83ed5138819e2e77f9b77cba810c42c7e8cc
Orlando Sergio Dominguez <serggio@msn.com>
2016-10-05 08:22:19 -0300

    Versión 0.0.21: Cambios en el diputados.

--- public/application/application.js
+++ public/application/application.js
@@ -4,7 +4,12 @@ angular.module('legislaturaweb',
 		,'mgcrea.ngStrap'
 		//addmodule.
 	])
-	.config(function($routeProvider){
+	.config(function($routeProvider,$httpProvider){
+		if(!$httpProvider.defaults.headers.get) $httpProvider.defaults.headers.get = {};
+		$httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+		$httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+		$httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+		
 	    $routeProvider
 	    	.when('/',{
 	    		redirectTo:'/home'

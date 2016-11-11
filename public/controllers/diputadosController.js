002268e9d7f4d46ab0c8c454165e54b0e7e8d405
Orlando Sergio Dominguez <serggio@msn.com>
2016-11-11 06:56:35 -0300

    Versión 0.0.32: Vista y controlador diputados.

--- public/controllers/diputadosController.js
+++ public/controllers/diputadosController.js
@@ -1,6 +1,22 @@
 angular
 		.module('legislaturaweb')
 		.controller('diputadosController',function($scope,$http){
-				// Mostrar media bar.
-				$('#mediaBar').hide();
+			 // Rutas.
+			 $scope.routeToDiputadosContent = '/rest/institucion.php/diputados';
+
+			 // Inicilizador.
+			 $scope.init = ()=>{
+				 $scope.getWidgetContentDiputados();
+			 };
+
+			 // Widget Content: Diputados.
+			 $scope.getWidgetContentDiputados = ()=>{
+				 $http
+				 	.get($scope.routeToDiputadosContent)
+					.success((json)=>{if(json.result) $scope.diputadosContent = json.rows;})
+					.error(()=>{ console.log($scope.routeToDiputadosContent+' : No Data');});
+			 };
+
+			 // Inicializar.
+			 $scope.init();
 		});

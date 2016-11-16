a7399eae9f0f30d2ae44c3fe578add6165842948
Orlando Sergio Dominguez <serggio@msn.com>
2016-11-16 09:39:14 -0300

    Versión 0.0.38: Autoridades.

--- public/controllers/autoridadesController.js
+++ public/controllers/autoridadesController.js
@@ -1,6 +1,29 @@
 angular
 		.module('legislaturaweb')
 		.controller('autoridadesController',function($scope,$rootScope,$http){
-			// Mostrar media bar.
-			$('#mediaBar').hide();
+			// Inicializadora.
+			$scope.init = ()=>{
+				$rootScope.mediaBar=false;
+				$scope.resetVars();
+				$scope.showPresidente();
+			};
+
+			// Resetear variables.
+			$scope.resetVars = ()=>{
+				$scope.varPresidente = false;
+				$scope.varVicePresidente1 = false;
+				$scope.varVicePresidente2 = false;
+				$scope.varSecParla = false;
+				$scope.varSecAdmin = false;
+			};
+
+			// Mostrar páginas.
+			$scope.showPresidente 		= ()=>{$scope.resetVars();$scope.varPresidente=true;};
+			$scope.showVicepresidente1 	= ()=>{$scope.resetVars();$scope.varVicePresidente1=true;};
+			$scope.showVicepresidente2 	= ()=>{$scope.resetVars();$scope.varVicePresidente2=true;};
+			$scope.showSecParla 		= ()=>{$scope.resetVars();$scope.varSecParla=true;};
+			$scope.showSecAdmin 		= ()=>{$scope.resetVars();$scope.varSecAdmin=true;};
+			
+			// Inicializar.
+			$scope.init();
 		});
